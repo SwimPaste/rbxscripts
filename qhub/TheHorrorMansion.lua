ba72391f614ad57dd6bd80af4fd6ab0d598f82a7
Denis <lameerr@vk.com>
2022-10-26 17:43:52 +0700

    - Better code, naming - Added comments

--- qhub/TheHorrorMansion.lua
+++ qhub/TheHorrorMansion.lua
@@ -1,55 +1,63 @@
 --ORION SEX
-getgenv().candyspawn = game:GetService("Workspace")["Halloween Decor"]["Candy Spawns"]
-getgenv().candy = candyspawn:GetChildren()
-getgenv().lp = game.Players.LocalPlayer
-getgenv().Farm = false
+getgenv().CandySpawn = game:GetService("Workspace")["Halloween Decor"]["Candy Spawns"]
+getgenv().Candy = CandySpawn:GetChildren()
+getgenv().LPlayer = game.Players.LocalPlayer
+getgenv().CandyFarmState = false
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 function ex_init()
 
-local MainWindow = OrionLib:MakeWindow({Name = "qHub for The Horror Mansion", HidePremium = false, SaveConfig = true, ConfigFolder = "qHub_thm", IntroEnabled = true, IntroText = "quell's hub"})
-local FarmTab = MainWindow:MakeTab({
-    Name = "Farm",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-local SettingsTab = MainWindow:MakeTab({
-    Name = "Settings",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
-SettingsTab:AddButton({
-    Name = "Destroy GUI",
-    Callback = function()
-        OrionLib:Destroy()
-    end
-})
-
-FarmTab:AddToggle({
-	Name = "Candy Farm",
-	Default = false,
-	Callback = function(Value)
-        getgenv().Farm = Value
-        doFarm()
-        print(Farm)
-	end
-})
-function doFarm()
-    spawn(function()
-        while Farm do
-            for i, v in pairs(candy) do
-                v.Position = lp.Character.Torso.Position
-            task.wait()
+    local MainWindow = OrionLib:MakeWindow({Name = "qHub for The Horror Mansion", HidePremium = false, SaveConfig = true, ConfigFolder = "qHub_thm", IntroEnabled = true, IntroText = "quell's hub"})
+    
+    --Creating functions
+    function doCandyFarm()
+        spawn(function()
+            while CandyFarmState do
+                for i, v in pairs(Candy) do
+                    v.Position = LPlayer.Character.Torso.Position
+                    task.wait()
+                end
             end
-       end
-    end)
-end
-OrionLib:Init()
+        end)
+    end
+
+    --Creating "Farm" tab and it's contents
+    local FarmTab = MainWindow:MakeTab({
+        Name = "Farm",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    FarmTab:AddToggle({
+        Name = "Candy Farm",
+        Default = false,
+        Callback = function(Value)
+            getgenv().Farm = Value
+            doCandyFarm()
+            print(CandyFarmState)
+        end
+    })
+
+    --Creating "Settings" tab and it's contents
+     local SettingsTab = MainWindow:MakeTab({
+        Name = "Settings",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    SettingsTab:AddButton({
+        Name = "Destroy GUI",
+        Callback = function()
+            OrionLib:Destroy()
+        end
+    })
+
+    OrionLib:Init()
 
 end
 
+--Checking game and loading universal hub if needed
 if game.GameId ~= 203829788 then
     OrionLib:MakeNotification({
         Name = "Error!",
@@ -58,7 +66,7 @@ if game.GameId ~= 203829788 then
         Time = 5
     })
     OrionLib:Destroy()
-loadstring(game:HttpGet(('https://github.com/SwimPaste/rbxscripts/raw/main/qhub/universal.lua')))()
+    loadstring(game:HttpGet(('https://github.com/SwimPaste/rbxscripts/raw/main/qhub/universal.lua')))()
 else
     ex_init()
 end
