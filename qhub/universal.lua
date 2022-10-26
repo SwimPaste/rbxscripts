0bc13a0ae166f408395729d237e470825e5fa823
SwimPaste <65006945+SwimPaste@users.noreply.github.com>
2022-10-26 19:16:02 +0900

    Create universal.lua

---
+++ qhub/universal.lua
@@ -0,0 +1,44 @@
+--ORION SEX
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+function ex_init()
+
+local MainWindow = OrionLib:MakeWindow({Name = "qHub Universal", HidePremium = false, SaveConfig = true, ConfigFolder = "qHub_thm", IntroEnabled = true, IntroText = "quell's hub"})
+local VisualsTab = MainWindow:MakeTab({
+    Name = "Visuals",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+local SettingsTab = MainWindow:MakeTab({
+    Name = "Settings",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+SettingsTab:AddButton({
+    Name = "Destroy GUI",
+    Callback = function()
+        OrionLib:Destroy()
+    end
+})
+
+VisualsTab:AddButton({
+    Name = "HD Graphics",
+    Callback = function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/SwimPaste/rbxscripts/main/hdgraphLOADER.lua'),true))()
+    end
+})
+function doFarm()
+    spawn(function()
+        while Farm do
+            for i, v in pairs(candy) do
+                v.Position = lp.Character.Torso.Position
+            task.wait()
+            end
+       end
+    end)
+end
+OrionLib:Init()
+
+end
+ex_init()
